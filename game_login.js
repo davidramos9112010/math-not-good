5dce8d5df8e9a5efd1d2460996ac49bc65aa03d2
davidramos9112010 <81586209+davidramos9112010@users.noreply.github.com>
2021-09-01 04:32:49 +0530

    Add files via upload

--- game_login.js
+++ game_login.js
@@ -8,12 +8,14 @@ window.location="game_page.html"
 function send(){
 num1=document.getElementById("num1").value;
 num2=document.getElementById("num2").value;
-
-
-
-
-
-
+actual_answer=parseInt(num1) * parseInt(num2);
+question_num="<h4>"+ num1 + "x" + num2 +"</h4>";
+input_box="<br> Answer:<input type'text' id='input_check_box'>";
+check_button="<br><br><button class="btn btn-info" onclick ="check()">check </button>;
+row=question_num + input_box + check_button ;
+document.getElementById("output").innerHTML = row;
+document.getElementById("num1").value = "";
+document.getElementById("num2").value = "";
 
 
 
